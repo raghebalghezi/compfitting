7b7692a71ab5c20b3840befbe9d155d6d6d8940c
Ragheb Al-Ghezi <raghebalghezi@gmail.com>
2018-07-20 19:02:34 -0700

    Update compfit.py
    changed the entire function. now, it's the correct implementation of partial derv of the proposed objective function.

--- compfit.py
+++ compfit.py
@@ -16,25 +16,30 @@ compositional_dict = dict() # dictionary of words extracted from wikitionary
 syn_dict = dict() #synonyms
 ant_dict = dict() #antonyms
 
+def norm(v):
+    return v / np.sqrt((v**2 + 1e-6).sum())
+def dis(u,v):
+    return np.dot(u,v)
+
 print("Loading data...")
-with open('./linguistic_constraints/glove.6B.300d.txt','r',encoding="utf-8") as f:
+with open('../glove.6B/glove.6B.300d.txt','r',encoding="utf-8") as f:
     for line in f:
     	v = np.float32(line.split()[1:])
-    	word_vectors[line.split()[0].strip()] = v/np.linalg.norm(v)
-
+    	word_vectors[line.split()[0].strip()] = norm(v)
 with open("./linguistic_constraints/compositional.txt",'r',encoding="utf-8") as file:
     for line in file:
         compositional_dict[line.split()[0].strip()]=line.split()[1].strip().split(",")
         
-with open("./linguistic_constraints/wordnet-synonyms+.txt",'r',encoding="utf-8") as file:
+with open("./linguistic_constraints/ppdb_synonyms.txt",'r',encoding="utf-8") as file:
     for line in file:
         syn_dict[line.strip().split()[0]] = line.strip().split()[1:]
 
-with open("./linguistic_constraints/wordnet_antonyms.txt",'r',encoding="utf-8") as file:
+with open("./linguistic_constraints/ppdb_antonyms.txt",'r',encoding="utf-8") as file:
     for line in file:
         ant_dict[line.strip().split()[0]] = line.strip().split()[1:]
-
-def compoFitting(X, syn_dict,ant_dict,compositional_dict, n_iter=10):
+        
+     
+def enhance_euclid(word_vectors,syn_dict,ant_dict,compositional_dict):
     '''
     Enhances Word vector (GloVe) using synonyms, antonyms and etymological components.
     ==================================================================================
@@ -44,47 +49,72 @@ def compoFitting(X, syn_dict,ant_dict,compositional_dict, n_iter=10):
   \hat{q_i} +\sum_{r : (i,r) \in ANT} \gamma_{ir} q_r + \sum_{c : (i,c) \in COMP} \delta_{ic} \sum_{1:(c)}q_c } 
 {\sum_{j : (i,j) \in SYN} \beta_{ij} + \sum_{r : (i,r) \in ANT} \gamma_{ir} + \sum_{c : (i,c) \in COMP} \delta_{ic} + \alpha_i}
 
-    X: Distribuational Vector
+    word_vectors: Distribuational Vector
     syn_dict: dictionary of key(word):values(list of synonyms) from WordNet
     ant_dict: dictionary of key(word):values(list of antonyms) from WordNet
     compositional_dict: dictionary of key(word):values(list of components) from Wiktionary.org
     alpha: distributional preservation weight. Ideally, it should be 1.
     beta: synonym vector weight. Should be 1/len(neighbors) ?
-    gamma: antonym vector weight. Should be zero as we want to repel from antonyms ? 
-    delta: compositional weight. set to 1, but the ideal value is unknown.
+    gamma: antonym vector weight. Should be 1/len(neighbors) ?
+    delta: compositional weight. set to 1/len(components)
     n_iter: Number of iteration, defaulted to 10
     
-    Returns : Dictionary of ComFitted Vectors
+    Returns : Dictionary of enhanced Vectors
     '''
+    Y = word_vectors.copy()
+    Y_prev = word_vectors.copy()
     
-    Y = X.copy() #intialize w/ X vectors
-    Y_prev = Y.copy()
-    alpha = lambda x:1
-    beta = lambda x:len(syn_dict[x]) if len(syn_dict[x])!=0 else 1
-    gamma = lambda x:0.1
-    delta = lambda x:1
-    #normalize = lambda x:(x/np.linalg.norm(x)) + 1e-6
-
-    for iteration in range(1, n_iter+1):
-        
-        for idx,word in enumerate(X):
+    for i in range(1,11):
+        for w in word_vectors.keys():
             try:
-                t1 = Y_prev[word] * alpha(word)
-                t1 = t1 + np.array([Y_prev[j] * beta(word) for j in syn_dict[word]]).sum(axis=0)
-                t2 = t1 - np.array([Y_prev[r] * gamma(word) for r in ant_dict[word]]).sum(axis=0)
-                t3 = t2 + np.array([Y_prev[c] for c in compositional_dict[word]]).sum(axis=0)
-                Y_prev[word] = t3/( beta(word) + alpha(word) + gamma(word) + delta(word))
+                
+                word_vectors[w] = word_vectors[w] * 2 #2 is beta
+                for j in set(syn_dict[w]):
+                    word_vectors[w] += word_vectors[j]
+                word_vectors[w] = word_vectors[w] / 2 # weighted sum
+                
+                word_vectors[w] = word_vectors[w] * 2
+                for r in set(ant_dict[w]):
+                    word_vectors[w] -= word_vectors[j]
+                word_vectors[w] = word_vectors[w] / 2
+                
+                word_vectors[w] = word_vectors[w] * 2
+                q_c = np.zeros(300)
     
+                for c in set(compositional_dict[w]):
+                    q_c += word_vectors[c]
+                q_c = q_c / len(word_vectors[c])
+                word_vectors[w] += q_c
+                word_vectors[w] = word_vectors[w] / 2
+
             except KeyError:
                 pass
-    return Y_prev 
+            #print("before",w, Y_prev[w])
+        Y[w] -= word_vectors[w]
+            #print("after",w, Y[w])
+    return Y
 
-retrofitted_word_vector = compoFitting(word_vectors, syn_dict,ant_dict,compositional_dict, n_iter=10)
-results_original = evaluate_on_all(word_vectors) #evaluate original vectors
-results_retrofitted = evaluate_on_all(retrofitted_word_vector) # evaluate after retrofitting
+Y = enhance_euclid(word_vectors,syn_dict,ant_dict,compositional_dict)
+results_retrofitted = evaluate_on_all(Y)
+print(results_retrofitted)
 
-print(results_original.to_dict())
-print(results_retrofitted.to_dict())
-
-
-#
+'''
+{'AP': {0: 0.63681592039800994},
+ 'BLESS': {0: 0.82000000000000006},
+ 'Battig': {0: 0.41693748805199771},
+ 'ESSLI_1a': {0: 0.77272727272727271},
+ 'ESSLI_2b': {0: 0.82500000000000007},
+ 'ESSLI_2c': {0: 0.64444444444444449},
+ 'Google': {0: 0.10284486287351617},
+ 'MEN': {0: 0.73746469698055173},
+ 'MSR': {0: 0.086999999999999994},
+ 'MTurk': {0: 0.63318199788472018},
+ 'RG65': {0: 0.76952497886121318},
+ 'RW': {0: 0.36701861264054064},
+ 'SemEval2012_2': {0: 0.16995896694889304},
+ 'SimLex999': {0: 0.37050035710869067},
+ 'TR9856': {0: 0.099725736554407501},
+ 'WS353': {0: 0.54355827297629189},
+ 'WS353R': {0: 0.47754204101544628},
+ 'WS353S': {0: 0.66245573499038346}}
+ '''
